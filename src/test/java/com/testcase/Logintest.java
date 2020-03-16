2e5d5d290bceea63dd0a69ff975ddd8ee5aa5a7d
unknown <nileshsalunkhe2011@gmail.com>
2020-03-16 14:22:29 +0530

    Hello

---
+++ src/test/java/com/testcase/Logintest.java
@@ -0,0 +1,32 @@
+package com.testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Logintest {
+	WebDriver driver;
+	@BeforeSuite
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("C:\\Users\\Admin\\Desktop\\Sellenium\\Offline Website\\index.html");
+		driver.manage().window().maximize();
+	}
+@Test
+public void test01()
+{
+	driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+	driver.findElement(By.id("password")).sendKeys("123456");
+	driver.findElement(By.xpath("//button[@type='submit']")).click();
+}
+@Test
+public void test02()
+{
+	String title=driver.getTitle();
+	Assert.assertEquals("JavaByKiran | Dashboard", title);
+}
+}
